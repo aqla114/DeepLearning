6e43958f3044c33b6e04f1eec3b788dec2573cf6
denjo <aqla114@yahoo.co.jp>
2017-05-31 19:45:39 +0900

    lecture4 complete at University

--- Function.py
+++ Function.py
@@ -36,5 +36,21 @@ def NumericalDiff(f, x):
     return (f(x+h) - f(x-h)) / 2*h
 
 def NumericalGradient(f, x):
-    h = 1e-4
-    grad = np.zeros_like(x) #xと同じ形状の配列を生成する
+    h = 1e-4 # 0.0001
+    grad = np.zeros_like(x)
+
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        idx = it.multi_index
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x) # f(x+h)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x) # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+
+        x[idx] = tmp_val # 値を元に戻す
+        it.iternext()
+
+    return grad
---
+++ Simplenet.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+import sys, os
+sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
+import numpy as np
+from Function import Softmax, CrossEntropyError
+from Function import NumericalGradient
+
+
+class simpleNet:
+    def __init__(self):
+        self.W = np.random.randn(2,3)
+
+    def predict(self, x):
+        return np.dot(x, self.W)
+
+    def loss(self, x, t):
+        z = self.predict(x)
+        y = Softmax(z)
+        loss = CrossEntropyError(y, t)
+
+        return loss
